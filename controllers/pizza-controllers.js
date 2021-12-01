53f63e9a7dbfe5270b63a1ab15c164d484ced17f
David F Mueth <78443941+DoctorMuch@users.noreply.github.com>
2021-12-01 10:59:17 -0600

    started over to get more coding experience building out

--- controllers/pizza-controllers.js
+++ controllers/pizza-controllers.js
@@ -3,29 +3,18 @@ const { Pizza } = require('../models');
 const pizzaController = {
   getAllPizza(req, res) {
     Pizza.find({})
-      .populate({
-        path: 'comments',
-        select: '-__v'
-      })
-      .select('-__v')
-      .sort({ _id: -1 })
       .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
         res.status(400).json(err);
       });
-  }, 
+  },
 
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
-      .populate({
-        path: 'comments',
-        select: '-__v'
-      })
-      .select(' -__v')
       .then(dbPizzaData => {
-        if(!dbPizzaData) {
-          res.status(404).json({ message: 'No pizza found with this id!' });
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No such pizza found!' });
           return;
         }
         res.json(dbPizzaData);
@@ -36,32 +25,32 @@ const pizzaController = {
       });
   },
 
-  createPizza({ body }, res) {
+  createNewPizza({ body }, res) {
     Pizza.create(body)
       .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => res.status(400).json(err));
   },
 
   updatePizza({ params, body }, res) {
-    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true})
       .then(dbPizzaData => {
         if(!dbPizzaData) {
-          res.status(404).json({ message: 'No pizza found by this id!' });
+          res.status(404).json({ message: 'No such pizza was found!' });
           return;
         }
         res.json(dbPizzaData);
       })
-      .catch(err => res.status(400).json(err)); 
+      .catch(err => res.status(400).json(err));
   },
 
-  deletePizza({ params }, res) {
+  removePizza({ params }, res) {
     Pizza.findOneAndDelete({ _id: params.id })
       .then(dbPizzaData => {
-        if (!dbPizzaData) {
-          res.status(404).json({ message: 'No pizza found by this id!' });
+        if(!dbPizzaData) {
+          res.status(404).json({ message: 'No such pizza found to remove!' });
           return;
         }
-        res.json(dbPizzaData)
+        res.json(dbPizzaData);
       })
       .catch(err => res.status(400).json(err));
   }
--- models/Pizza.js
+++ models/Pizza.js
@@ -1,5 +1,4 @@
 const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
 
 const PizzaSchema = new Schema({
   pizzaName: {
@@ -10,32 +9,13 @@ const PizzaSchema = new Schema({
   },
   createdAt: {
     type: Date,
-    default: Date.now,
-    get: (createdAtVal) => dateFormat(createdAtVal)
+    default: Date.now
   },
   size: {
     type: String,
     default: 'Large'
   },
-  toppings: [],
-  comments: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Comment'
-    }
-  ]
-},
-{
-  toJSON: {
-    virtuals: true,
-    getters: true
-  },
-  id: false
-  }
-);
-
-PizzaSchema.virtual('commentCount').get(function() {
-  return this.comments.length;
+  toppings: []
 });
 
 const Pizza = model('Pizza', PizzaSchema);
